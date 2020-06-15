d92cf3e557f8b92b2a24c96750e2815535128596
Balandina, Sofya <sofya.balandina@intel.com>
2020-06-15 20:15:51 +0300

    Updates

---
+++ precache-manifest.5446956ce02cb925671ede81ca9f7045.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fa63b2a71bde52bb41a9073f35392bd",
+    "url": "/git-hub-page/index.html"
+  },
+  {
+    "revision": "232bc399aeaf492a0888",
+    "url": "/git-hub-page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "af396342e7a2ccf440d5",
+    "url": "/git-hub-page/static/js/2.121aef46.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/git-hub-page/static/js/2.121aef46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "232bc399aeaf492a0888",
+    "url": "/git-hub-page/static/js/main.28040ae1.chunk.js"
+  },
+  {
+    "revision": "1465007a360361d1564a",
+    "url": "/git-hub-page/static/js/runtime-main.89271215.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/git-hub-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
